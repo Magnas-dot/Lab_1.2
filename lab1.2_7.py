02c3a66805e9e20c919ac23f3ef9913db12b7ef7
Sangam Paudel <sangampaudel47@gmail.com>
2025-07-06 11:34:27 +0545

    Create lab1.2_7.py

---
+++ lab1.2_7.py
@@ -0,0 +1,13 @@
+#7. Write a program to generate the Fibonacci sequence up to n terms
+n = int(input("Enter the number of terms: "))
+a, b = 0, 1
+count = 0
+if n <= 0:
+    print("Please enter a positive integer.")
+else:
+    print("Fibonacci Series:")
+    while count < n:
+        print(a, end=' ')
+        a, b = b, a + b
+        count += 1
+
